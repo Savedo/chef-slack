30a663c9f55f8166fb2e0dd234cd4bf01af55702
Timur Khafizov <hangmany@gmail.com>
2016-08-22 14:14:26 +0200

    Allow more options for Slack

--- providers/notify.rb
+++ providers/notify.rb
@@ -5,16 +5,21 @@ end
 use_inline_resources
 
 action :notify do
-  if node['slack']['webhook_url']
-    slack = Slack::Notifier.new(node['slack']['webhook_url'])
-  else
-    slack = Slack::Notifier.new(new_resource.webhook_url)
-  end
+  webhook_url = new_resource.webhook_url || node["slack"]["webhook_url"]
+  slack_notifier = Slack::Notifier.new(webhook_url)
 
-    new_resource.channels.each do |channel|
-      options = {}
-      options["channel"]    = channel                  if new_resource.channels
-      options["username"]   = new_resource.username    if new_resource.username
-      slack.ping(new_resource.message,options)
+  channels = new_resource.channels || [nil]
+  allowed_options = %i(message username attachments icon_emoji icon_url)
+
+  channels.each do |channel|
+    options = {}.tap do |options|
+      options[:channel] = channel
+
+      allowed_options.each do |option|
+        options[option] = new_resource.public_send(option)
+      end
+    end.reject { |_k, v| v.nil? }
+
+    slack_notifier.ping(new_resource.message, options)
   end
 end
--- resources/notify.rb
+++ resources/notify.rb
@@ -1,10 +1,13 @@
-actions        :notify
+actions :notify
 default_action :notify
 
-attribute      :message,     :kind_of => String, :name_attribute => true
-attribute      :channels,    :kind_of => Array
-attribute      :username,    :kind_of => String
-attribute      :webhook_url, :kind_of => String
+attribute :message, kind_of: String, name_attribute: true
+attribute :channels, kind_of: Array
+attribute :username, kind_of: String
+attribute :webhook_url, kind_of: String
+attribute :attachments, kind_of: Array
+attribute :icon_emoji, kind_of: String
+attribute :icon_url, kind_of: String
 
 def initialize(*args)
   super
