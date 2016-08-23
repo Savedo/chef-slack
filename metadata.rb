e64abccc67986e81efa91536b144cdfef0fd4a29
Timur Khafizov <hangmany@gmail.com>
2016-08-23 12:29:50 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'slack'
-maintainer       'Ian Henry'
-maintainer_email 'ihenry@chef.io'
-license          'Apache 2.0'
-description      'slack LWRP for notifying slack.com channels'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+name             "slack"
+maintainer       "Savedo IT-Team"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Slack LWRP for notifying slack.com channels"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.1.1"
 
 %w{redhat centos scientific fedora debian ubuntu arch freebsd amazon}.each do |os|
   supports os
